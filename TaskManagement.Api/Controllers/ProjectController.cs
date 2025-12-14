5e2e965044a1bb84fa4a137aede98df18d52691f
oezal <akdenizozal@gmail.com>
2025-12-14 01:40:46 +0100

    Add ProjectController with CRUD endpoints.

--- TaskManagement.Api/Controllers/ProjectController.cs
+++ TaskManagement.Api/Controllers/ProjectController.cs
@@ -1,12 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using TaskManagement.Application.Interfaces;
+using TaskManagement.Domain.Entities;
 
 namespace TaskManagement.Api.Controllers
 {
-    public class ProjectController : Controller
+    [ApiController]
+    [Route("api/projects")]
+    public class ProjectController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IProjectService _projectService;
+
+        public ProjectController(IProjectService projectService)
         {
-            return View();
+            _projectService = projectService;
         }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(Project project)
+        {
+            var createdProject = await _projectService.CreateAsync(project);
+            return CreatedAtAction(nameof(GetById), new { id = createdProject.Id }, createdProject);
+        }
+
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var projects = await _projectService.GetAllAsync();
+            return Ok(projects);
+        }
+
+        [HttpGet("GetById {id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var project = await _projectService.GeByIdAsync(id);
+            if (project == null) return NotFound();
+
+            return Ok(project);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, Project project)
+        {
+            if(id != project.Id) return BadRequest("Project ID mismatch");
+
+            var updated = await _projectService.UpdateAsync(project);
+
+            if (!updated) return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete (Guid id)
+        {
+            var deleted = await _projectService.DeleteAsync(id);
+            if (!deleted) return NotFound();
+
+            return NoContent();
+        }
+
+
     }
 }
